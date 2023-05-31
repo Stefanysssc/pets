76eb114beab49def7d7b3e8493fcb321a705afa8
StefanySuzanne <STEFANYSUZANNE18@GMAIL.COM>
2023-05-31 14:28:49 -0300

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
 
 
